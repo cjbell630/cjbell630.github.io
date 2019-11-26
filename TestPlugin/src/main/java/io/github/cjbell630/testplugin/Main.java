50f450dfb5c44ddaed5bddcda807b6a02f9aed38
Connor Bell <cjbell630@users.noreply.github.com>
2019-11-25 22:31:49 -0500

    bwejf

---
+++ TestPlugin/src/main/java/io/github/cjbell630/testplugin/Main.java
@@ -0,0 +1,7 @@
+package io.github.cjbell630.testplugin;
+
+public class Main {
+    public static void main(String[] args) {
+        TestPlugin tp = new TestPlugin();
+    }
+}
--- TestPlugin/src/main/java/io/github/cjbell630/testplugin/TestPlugin.java
+++ TestPlugin/src/main/java/io/github/cjbell630/testplugin/TestPlugin.java
@@ -14,6 +14,9 @@ import java.util.HashMap;
 import java.util.Objects;
 
 public final class TestPlugin extends JavaPlugin {
+    public TestPlugin(){
+        onEnable();
+    }
     private HashMap<String, String> playerList = new HashMap<String, String>();
 
     @Override
