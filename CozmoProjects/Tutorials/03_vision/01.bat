7583f18213dce4f24e589f38df0a207b4a9d46fe
Connor Bell <cjbell630@users.noreply.github.com>
2020-10-07 19:35:04 -0400

    moved files

--- CozmoProjects/Tutorials/03_vision/01.bat
+++
@@ -1,3 +0,0 @@
-D:
-cd D:\cb106\Documents\Cozmo\cozmo_sdk_examples_1.4.10\cozmo_sdk_examples_1.4.10\tutorials\03_vision
-py 01_light_when_face.py
--- TestPlugin/src/main/java/io/github/cjbell630/testplugin/Main.java
+++
@@ -1,7 +0,0 @@
-package io.github.cjbell630.testplugin;
-
-public class Main {
-    public static void main(String[] args) {
-        TestPlugin tp = new TestPlugin();
-    }
-}
