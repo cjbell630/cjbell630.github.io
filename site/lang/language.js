4d0be9fd01ec54e5dc3666eaa35ff9141174e2f3
Connor Bell <ronnoclleb@gmail.com>
2023-01-11 18:15:58 -0500

    add projects

--- site/lang/language.js
+++ site/lang/language.js
@@ -12,6 +12,16 @@ const languages = {
         "TALL-INFINITY-HD-DESC": "A Unity remake of the PlayStation 1 puzzle game \"Tall Infinity\".",
         "YOSHI": "Yoshi NES Bot",
         "YOSHI-DESC": "A bot that plays the NES game \"Yoshi\".",
+        "KAREL": "Karel the Robot - Python",
+        "KAREL-DESC": "A Python implementation of Karel the Robot, a Java program designed to help learn programming.",
+        "PISOUND-GAMEPAD": "PiSound Gamepad",
+        "PISOUND-GAMEPAD-DESC": "A program that allows you to use a Wii U gamepad to control PiSound software.",
+        "ANKI": "Anki Cards + Tools",
+        "ANKI-DESC": "A collection of cards and tools for learning Japanese with the Anki flashcard program.",
+        "JAVA-FX-TETRIS": "JavaFX Tetris",
+        "JAVA-FX-TETRIS-DESC": "A JavaFX implementation of the classic game Tetris.",
+        "THIS-SITE": "This Site",
+        "THIS-SITE-DESC": "The website you're on right now.",
     },
     "ja-JP": {
         "NAME": "コナー ・ ベル",
@@ -26,6 +36,16 @@ const languages = {
         "TALL-INFINITY-HD-DESC": "「TALL∞（トール・アンリミテッド）」というPlayStation1のパズルゲームのUnityリメイク。",
         "YOSHI": "ヨッシーのたまごボット",
         "YOSHI-DESC": "「ヨッシーのたまご」というファミコムゲームができるボット。",
+        "KAREL": "PythonのKarel the Robot",
+        "KAREL-DESC": "「Karel the Robot」というコードを学ぶJavaのプログラムのPython実装。",
+        "PISOUND-GAMEPAD": "PiSound Gamepad",
+        "PISOUND-GAMEPAD-DESC": "「PiSound」というソフトウェアをWii U GamePadで操作できるプログラム。",
+        "ANKI": "Ankiカードとツール",
+        "ANKI-DESC": "「Anki」というフラッシュカードプログラムで日本語を学ぶためのカードとツールの集まり。",
+        "JAVA-FX-TETRIS": "JavaFXテトリス",
+        "JAVA-FX-TETRIS-DESC": "JavaFXで作られたテトリスの実装。",
+        "THIS-SITE": "このサイト",
+        "THIS-SITE-DESC": "今見ているサイト。",
     }
 }
 
--- site/script/projects.js
+++ site/script/projects.js
@@ -1,9 +1,9 @@
 Languages = {
-    JavaScript: "https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg",
+    JS: "https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg",
     CPP: "https://upload.wikimedia.org/wikipedia/commons/1/18/ISO_C%2B%2B_Logo.svg",
     C: "https://upload.wikimedia.org/wikipedia/commons/1/18/C_Programming_Language.svg",
     Lua: "https://upload.wikimedia.org/wikipedia/commons/c/cf/Lua-Logo.svg",
-    Java: "https://upload.wikimedia.org/wikipedia/commons/3/30/Java_programming_language_logo.svg",
+    Java: "https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg",
     HTML: "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg",
     CSS: "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
     PHP: "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg",
@@ -65,12 +65,22 @@ const projects = [
     new Project("YOSHI", "YOSHI-DESC",
         [Languages.Lua], "google.com", "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
     ),
-    new Project("YOSHI", "YOSHI-DESC",
-        [Languages.Lua], "google.com", "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+    new Project("KAREL", "KAREL-DESC",
+        [Languages.Python], "google.com", "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
     ),
-    new Project("YOSHI", "YOSHI-DESC",
-        [Languages.Lua], "google.com", "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+    new Project("PISOUND-GAMEPAD", "PISOUND-GAMEPAD-DESC",
+        [Languages.CPP], "google.com", "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
     ),
+    new Project("ANKI", "ANKI-DESC",
+        [Languages.JS], "google.com", "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+    ),
+    new Project("JAVA-FX-TETRIS", "JAVA-FX-TETRIS-DESC",
+        [Languages.Java], "google.com", "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+    ),
+    new Project("THIS-SITE", "THIS-SITE-DESC",
+        [Languages.HTML, Languages.CSS, Languages.JS], "google.com", "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+    ),
+    // TODO Kotlin
 ];
 
 
